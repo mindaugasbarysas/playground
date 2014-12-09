f6d503143e203a301da7ddcf26b72b1b25688121
Mindaugas Barysas <mindaugas.barysas@ongr.io>
2014-12-09 12:14:56 +0200

    woopawoopittywoop

--- source/pull.py
+++ source/pull.py
@@ -1,6 +1,9 @@
 # -- autopull repos
 
+
+
 lines = [line.strip() for line in open('%s/sources/.repos' % (os.path.dirname(os.path.realpath(__file__))))]
 
 for line in lines:
-    os.system("cd sources; git clone %s" % (line))
+    os.system("cd sources; mkdir %s && cd %s; git init; git remote add –f origin %s; git config core.sparsecheckout true; echo "Resources/" >> .git/info/sparse-checkout; git pull;" % (line.split("/")[-1], line.split("/")[-1], line)
+
