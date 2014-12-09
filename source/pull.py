cc9ce15a4701579b3058a3abf2ade6e5b3525274
Mindaugas Barysas <mindaugas.barysas@ongr.io>
2014-12-09 12:25:56 +0200

    final prototype of doc compiler

--- source/pull.py
+++ source/pull.py
@@ -5,5 +5,6 @@
 lines = [line.strip() for line in open('%s/sources/.repos' % (os.path.dirname(os.path.realpath(__file__))))]
 
 for line in lines:
-    os.system("cd sources; mkdir %s; cd %s; git init; git remote add -f origin %s; git config core.sparsecheckout true; echo 'Resources/' > .git/info/sparse-checkout; git pull origin master;" % (line.split("/")[-1], line.split("/")[-1], line))
+    if ( line[0]!="#" ) :
+        os.system("cd sources; mkdir %s; cd %s; git init; git remote add -f origin %s; git config core.sparsecheckout true; echo 'Resources/' > .git/info/sparse-checkout; git pull origin master;" % (line.split("/")[-1], line.split("/")[-1], line))
 
