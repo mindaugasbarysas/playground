863c05a8a7134632d2749470a2ef3b3d95ed0a58
Mindaugas Barysas <mindaugas.barysas@ongr.io>
2014-12-09 11:32:01 +0200

    third prototype

--- source/conf.py
+++ source/conf.py
@@ -268,7 +268,7 @@ api_url = 'http://api.symfony.com/master/%s'
 
 # -- autopull repos
 
-lines = [line.strip() for line in open('sources/.repos')]
+lines = [line.strip() for line in open('%s/sources/.repos' % (os.path.dirname(os.path.realpath(__file__))))]
 
 for line in lines:
     os.system("cd sources; git clone %s" % (line))
