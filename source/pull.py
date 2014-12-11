3c2f08ad529ff0cb18ab88fda62c3d3eaa71115c
Mindaugas Barysas <mindaugas.barysas@ongr.io>
2014-12-11 11:10:35 +0200

    production

--- source/pull.py
+++ source/pull.py
@@ -8,4 +8,3 @@ for line in lines:
     if ( line[0]!="#" ) :
         os.system("cd sources; mkdir %s; cd %s; git init; git remote add -f origin %s; git config core.sparsecheckout true; echo 'Resources/doc' > .git/info/sparse-checkout; echo 'Resources/API' >> .git/info/sparse-checkout; git pull origin master;" % (line.split("/")[-1], line.split("/")[-1], line))
 
-### fake
